66239fe850a1d869c542d1e2caa7500032d06331
Gizmoring <arcanacoach@gmail.com>
2024-10-01 15:29:27 +0900

    history클래스생성

---
+++ src/main/java/com/example/godgame/history/GameRoomHistory.java
@@ -0,0 +1,28 @@
+package com.example.godgame.history;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class GameRoomHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long gameRoomHistoryId;
+
+    @Column
+    private int currentMember;
+
+    @Column
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    @Column
+    private LocalDateTime createdAt = LocalDateTime.now();
+    
+}
