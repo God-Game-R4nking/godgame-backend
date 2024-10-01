0d4d239ff20123da586cb4fea2ec0a05965dc578
YoungJunRoh <shdudwns59@gmail.com>
2024-10-01 17:18:20 +0900

    feat/memberV3

--- src/main/java/com/example/godgame/member/controller/MemberController.java
+++ src/main/java/com/example/godgame/member/controller/MemberController.java
@@ -100,17 +100,5 @@ public class MemberController {
                 new SingleResponseDto<>(responseDto), HttpStatus.OK);
     }
 
-    @PostMapping("/verify-resident-registration")
-    public ResponseEntity<Boolean> verifyResidentRegistration(
-            @RequestBody MemberDto.ResidentVerificationRequest request) {
-        boolean isValid = memberService.verifyResidentRegistration(
-                request.getPhoneNo(),
-                request.getUserName(),
-                request.getIdentity(),
-                request.getIssueDate()
-        );
-        return ResponseEntity.ok(isValid);
-    }
-
 
 }
