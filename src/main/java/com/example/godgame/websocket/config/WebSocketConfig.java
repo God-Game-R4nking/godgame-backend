311d09f8f32c5b6eadb1885f12e087b93fae3a85
YoungJunRoh <shdudwns59@gmail.com>
2024-10-02 20:19:14 +0900

    Web Socket Basic

--- src/main/java/com/example/godgame/websocket/config/WebSocketConfig.java
+++ src/main/java/com/example/godgame/websocket/config/WebSocketConfig.java
@@ -1,23 +1,35 @@
-//package com.example.godgame.websocket.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-//
-//@Configuration
+package com.example.godgame.websocket.config;
+
+import com.example.godgame.websocket.webchat.MyHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
 //@EnableWebSocketMessageBroker
-//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-//
-//
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
 //    @Override
 //    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//
+//        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
 //    }
 //
 //    @Override
 //    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+//       registry.enableSimpleBroker("/sub");
+//       registry.setApplicationDestinationPrefixes("/pub");
 //    }
-//}
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/myHandler").setAllowedOriginPatterns("*");
+    }
+
+    @Bean
+    public WebSocketHandler myHandler(){
+        return new MyHandler();
+    }
+}
